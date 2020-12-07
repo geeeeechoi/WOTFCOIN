fdeabbddd33e26ab5eab6cd359144e6628831e65
geeeeechoi <geeeeechoi@gmail.com>
2020-12-07 16:39:32 -0600

    add erc721 stuff

--- app.js
+++ app.js
@@ -7,9 +7,54 @@ const contractAddress = "0xd7ea705d009157326f31890fce0b47739d3225ce";
 const contractABI = [
     "function reward(uint256 amt) public"
 ];
+const contractAddress721 = "0x722eE1576Aa33aC8FE71EC45f32DdF4c3b56571E";
+const contractABI721 = [
+  "function awardItem(address player, string memory tokenURI) public",
+  "function balanceOf(address account) public view returns (uint256)",
+  "function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256)",
+  "function totalSupply() public view returns (uint256)",
+  "function tokenByIndex(uint256 index) public view returns (uint256)",
+  "function safeTransferFrom(address from, address to, uint256 tokenId) public",
+  "function tokenURI(uint256 tokenId) public view returns (string memory)"
+];
+
 const contract =  new ethers.Contract(contractAddress, contractABI, provider);
 const tokenWithSigner = contract.connect(signer);
+
+const contract721 = new ethers.Contract(contractAddress721, contractABI721, provider);
+const tokenWithSigner721 = contract721.connect(signer);
 // tokenWithSigner.reward(10);
+
+const secretWord = "cool";
+let address;
+const badgeURI = `{"imageURL": "https://linktothepng.com/badge.png"}`
+
+
+main();
+
+
+async function main() {
+  address = await signer.getAddress();
+  let balance = await contract721.balanceOf(address);
+  balance = +balance;
+
+
+  $('.secret-word-button').click(function(){
+
+    if(balance > 0) {
+      $('.secret-word-output').text("You are already a member");
+    } else {
+      let inputWord = $('.secret-word-input').val().toLowerCase();
+
+      if(inputWord == secretWord){
+        $('.secret-word-output').text("");
+        tokenWithSigner721.awardItem(address, badgeURI);
+      } else {
+        $('.secret-word-output').text("Incorrect word");
+      }
+    }
+  })
+}
 
 
 ///////////p5 code ///////////
@@ -83,8 +128,8 @@ function gotResult(error, results) {
 
   if(results[0].label == "religious" && results[0].confidence > 0.95 && shouldReward){
     shouldReward = false;
-    //tokenWithSigner.reward(10);
-    
+    tokenWithSigner.reward(10);
+
     }
   // Classifiy again!
   classifyVideo();
